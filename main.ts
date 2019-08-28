f808f896b26dc43ae86ed4ac5ce7fb5a2a48c147
guowushi <guowushi@qq.com>
2019-08-28 23:28:53 +0800

    modified

--- main.ts
+++ main.ts
@@ -18,32 +18,30 @@ namespace Si7021 {
     let  SI7021_MEASURE_TEMP_NOHOLD_CMD = 0xF3;
     let SI7021_MEASURE_HUM_NOHOLD_CMD  = 0xF5;
     let SI7021_USER_REG_DEFAULT        = 0x3A;
-    /**
-     *  Init 
-    */
+
     //% block="Init I2c Address"
     //% blockExternalInputs=true
     export function init(addr: number): void {
-        SI7021_ADDRESS = addr
+        SI7021_ADDRESS = addr;
     }
      //% block="Reset Sensor"
-    export function reset(){
+    export function reset():void {
         pins.i2cWriteNumber(SI7021_ADDRESS, SI7021_RESET_CMD,   NumberFormat.Int8LE,  false  )
     }
 
     //% block="Read  Temperature (BE)"
     export function readTemperature():number{
-        pins.i2cWriteNumber(   0,   0,   NumberFormat.Int8LE,    false  )
+        pins.i2cWriteNumber(SI7021_ADDRESS,   SI7021_MEASURE_TEMP_NOHOLD_CMD,   NumberFormat.Int8LE,    false  )
        
-      let rawdata= pins.i2cReadNumber(0, NumberFormat.Int16LE, false);
+      let rawdata= pins.i2cReadNumber(SI7021_ADDRESS, NumberFormat.Int16LE, false);
        _temperature = (((rawdata * 175.72) / 65536) - 46.85);
        return  _temperature;
 
     }
     //% block="Read  Humidity (BE)"
     export function readHumidity():number{
-        
-       let rawdata = pins.i2cReadNumber(0, NumberFormat.Int16LE, false);
+        pins.i2cWriteNumber(SI7021_ADDRESS,   SI7021_MEASURE_HUM_NOHOLD_CMD,   NumberFormat.Int8LE,    false  )
+       let rawdata = pins.i2cReadNumber(SI7021_ADDRESS, NumberFormat.Int16LE, false);
       
          _humidity   = (((rawdata * 125) / 65536) - 6);
         return _humidity;
